3e9443f90aa56d70561a11cd93271859c83ded5e
hznagui <houdayfahouayfa7@gmail.com>
2024-03-12 09:49:05 +0000

    here we goo

--- tst.py
+++ tst.py
@@ -1,12 +1,8 @@
 from pwn import *
 
-
-
-
-
 for i in range(100):
     p = remote("localhost", 6670)
-    p.sendline("PASS password")
+    p.sendline("PASS a")
     p.sendline("NICK nick{0}".format(i))
     p.sendline("USER user{0} 0 * abc".format(i))
     p.sendline("JOIN #channel{0}".format(i))
